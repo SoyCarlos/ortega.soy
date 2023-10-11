6b8ccc78b2dbad5aad8c91932a5c6bbc19dae381
Carlos Eduardo Ortega <31175269+SoyCarlos@users.noreply.github.com>
2023-10-11 12:22:04 -0700

    CrossOrigin fix

--- ortega-site/src/pages/index.tsx
+++ ortega-site/src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <meta name="description" content="Ortega" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Syne+Mono&display=swap" rel="stylesheet" />
       </Head>
       <main>
